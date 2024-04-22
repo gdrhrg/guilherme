15861620b154a6bd9eaca87e4c1c794808962fad
gdrhrg <166012555+gdrhrg@users.noreply.github.com>
2024-04-22 12:18:44 -0300

    22 de abril de 2024

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
-const tempoObjetivo2 = new Date("2024-10-05T00:00:00")
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00")
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo1 = new Date("2006-09-13T00:00:00")
+const tempoObjetivo2 = new Date("2023-05-06T00:00:00")
+const tempoObjetivo3 = new Date("2020-10-23T00:00:00")
+const tempoObjetivo4 = new Date("2019-11-12T00:00:00")
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 contadores[0].textContent = calculaTempo(tempos[0]);
@@ -38,22 +38,22 @@ function atualizaCronometro(){
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000); }
-    comecaCronometro();
-
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
-
-function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24)
-
-    segundos %=60;
-    minutos %=60;
-    horas %=24;
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-}
+    
+    
+    
+    function calculaTempo(tempoObjetivo){
+        let tempoAtual = new Date();
+        let tempoFinal = tempoObjetivo - tempoAtual;
+        let segundos = Math.floor(tempoFinal/1000);
+        let minutos = Math.floor(segundos/60);
+        let horas = Math.floor(minutos/60);
+        let dias = Math.floor(horas/24)
+        
+        segundos %=60;
+        minutos %=60;
+        horas %=24;
+        
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }
+            
+        comecaCronometro();
